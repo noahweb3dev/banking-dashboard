a46a43b9b0470cb5a8f3bc44ff082ef920e15222
sugarDonut <sugardonut1010@gmail.com>
2025-12-24 16:48:35 +0100

    refactor: remove BankProvider wrapper from DashboardLayout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import { Sidebar } from "@/components/layout/sidebar"
 import { TopBar } from "@/components/layout/topbar"
 import { MobileBottomNav } from "@/components/layout/mobile-bottom-nav"
-import { BankProvider } from "@/components/providers/bank-provider"
 
 export default function DashboardLayout({
     children,
@@ -9,18 +8,16 @@ export default function DashboardLayout({
     children: React.ReactNode
 }) {
     return (
-        <BankProvider>
-            <div className="flex min-h-screen">
-                <Sidebar />
+        <div className="flex min-h-screen">
+            <Sidebar />
 
-                <div className="flex flex-1 flex-col">
-                    <TopBar />
-                    <main className="flex-1 p-6 pb-24 md:pb-6">
-                        {children}
-                    </main>
-                    <MobileBottomNav />
-                </div>
+            <div className="flex flex-1 flex-col">
+                <TopBar />
+                <main className="flex-1 p-6 pb-24 md:pb-6">
+                    {children}
+                </main>
+                <MobileBottomNav />
             </div>
-        </BankProvider>
+        </div>
     )
 }
